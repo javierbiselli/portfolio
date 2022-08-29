42a937ee935adb037e7e7d1571f17a9a9eef3b2e
Javier Biselli <javierbiselli@gmail.com>
2022-08-29 01:33:26 -0300

    Form working

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,27 +1,42 @@
 import styles from "./contact.module.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_g2le82c",
+        "template_ygrdrbb",
+        e.target,
+        "V61t4y1LnMzqdr-T4"
+      )
+      .then((res) => {
+        res.status === 200 ? alert("Message send") : alert(res.text);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <section className={styles.contactContainer} id="Contact">
       <div id="ContactSection" className={styles.contactSectionScroll}></div>
-      <form>
+      <form onSubmit={sendEmail}>
         <div className={styles.formContainer}>
           <h4>Contact me</h4>
           <div className={styles.inputContainer}>
             <div className={styles.singleInputContainer}>
               <label htmlFor="name">Name</label>
-              <input type="text" />
+              <input type="text" name="name" />
             </div>
             <div className={styles.singleInputContainer}>
               <label htmlFor="email">Email</label>
-              <input type="email" />
+              <input type="email" name="email" />
             </div>
           </div>
           <div className={styles.textArea}>
             <label htmlFor="textarea">Message</label>
             <textarea name="textarea" id="textarea"></textarea>
           </div>
-          <input type="submit" name="" id="" />
+          <input type="submit" value="Send" className={styles.submitButton} />
         </div>
       </form>
       <div className={styles.socialContainer}>
