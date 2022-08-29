ad746154ebfe0e438cd36ad88380f91066546ebd
Javier Biselli <javierbiselli@gmail.com>
2022-08-29 17:45:03 -0300

    Added validations

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,20 +1,58 @@
 import styles from "./contact.module.css";
 import emailjs from "emailjs-com";
+import Joi from "joi";
 
 const Contact = () => {
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_g2le82c",
-        "template_ygrdrbb",
-        e.target,
-        "V61t4y1LnMzqdr-T4"
-      )
-      .then((res) => {
-        res.status === 200 ? alert("Message send") : alert(res.text);
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(3)
+      .max(20)
+      .regex(/^[a-zA-Z_ ]*$/)
+      .messages({
+        "string.pattern.base": "Name must contain only letters",
       })
-      .catch((err) => console.log(err));
+      .required(),
+    email: Joi.string()
+      .required()
+      .regex(
+        /^[a-z0-9]+(?:\.[a-z0-9]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+      )
+      .messages({
+        "string.pattern.base": "Invalid email",
+        "string.empty": "This field is required",
+      })
+      .required(),
+    message: Joi.string().required(),
+  });
+
+  const isValid = async (e) => {
+    return isValid;
+  };
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+    let formData = {
+      name: e.target[0].value,
+      email: e.target[1].value,
+      message: e.target[2].value,
+    };
+    console.log(formData);
+    const isValid = await schema.validate(formData);
+    if (isValid.error) {
+      alert(isValid.error.details[0].message);
+    } else {
+      emailjs
+        .sendForm(
+          "service_g2le82c",
+          "template_ygrdrbb",
+          e.target,
+          "V61t4y1LnMzqdr-T4"
+        )
+        .then((res) => {
+          res.status === 200 ? alert("Message send") : alert(res.text);
+        })
+        .catch((err) => console.log(err));
+    }
   };
   return (
     <section className={styles.contactContainer} id="Contact">
@@ -26,6 +64,7 @@ const Contact = () => {
             <div className={styles.singleInputContainer}>
               <label htmlFor="name">Name</label>
               <input type="text" name="name" />
+              {isValid() ? <div className={styles.error}>{}</div> : ""}
             </div>
             <div className={styles.singleInputContainer}>
               <label htmlFor="email">Email</label>
@@ -33,8 +72,8 @@ const Contact = () => {
             </div>
           </div>
           <div className={styles.textArea}>
-            <label htmlFor="textarea">Message</label>
-            <textarea name="textarea" id="textarea"></textarea>
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message"></textarea>
           </div>
           <input type="submit" value="Send" className={styles.submitButton} />
         </div>
